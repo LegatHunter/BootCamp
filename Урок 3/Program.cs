2e14affb4d22f62ccb4b15589f1a0850775215d2
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-08-02 15:49:24 +0300

    Залил на гит

---
+++ Урок 3/Program.cs
@@ -0,0 +1,8 @@
+﻿int [] array = new int [5];
+for (int i = 0; i < 5; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+System.Console.WriteLine(array[3]);
+// Сложность алгоритма О(1)
