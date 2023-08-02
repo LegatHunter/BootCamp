683cc2718b0aca292aa33c7678dff276f2d0385e
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-08-02 21:20:34 +0300

    Половина 6го урока

---
+++ Урок 6/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = 5;
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    array[i] = Random.Shared.Next(10);
+}
+System.Console.WriteLine($"[{String.Join(", ", array)}]");
+for (int k = 0; k < n - 1; k++)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            int temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
+    }
+}
+System.Console.WriteLine($"[{String.Join(", ", array)}]");
