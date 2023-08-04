c536e8401f117a09c6af9c5fdcee47a9e7b5e666
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-08-04 18:25:22 +0300

    Добавлена быстрая сортировка

---
+++ Урок 8/Program.cs
@@ -0,0 +1,49 @@
+﻿//Быстрая сортировка
+
+int[] QuickSort(int[] arr)
+{
+    if (arr.Length <= 1) return arr;
+    else
+    {
+        int pivot = arr[0];
+        int countLeft = 0;
+        int countRight = 0;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < pivot)
+            {
+                countLeft++;
+            }
+            else countRight++;
+        }
+        int[] left = new int[countLeft];
+        int[] right = new int[countRight];
+
+        int numLeft = 0;
+        int numRight = 0;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < pivot)
+            {
+                left[numLeft] = arr[i];
+                numLeft++;
+            }
+            else
+            {
+                right[numRight] = arr[i];
+                numRight++;
+            }
+        }
+
+        // int [] left = arr.Skip(1).Where(x => x < pivot).ToArr();
+        // int [] right = arr.Skip(1).Where(x => x >= pivot).ToArr();
+        return QuickSort(left).Concat(new int[] {pivot}).Concat(QuickSort(right)).ToArray();
+    }
+}
+int[] arr = {3, 4, 1, 6, 1, 2, 4, 6, 7};
+int[] arr_result = QuickSort(arr);
+
+foreach (var item in arr_result)
+{
+    System.Console.Write($"{item}");
+}
